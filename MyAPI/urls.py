35d7c76d49a5ad2c1c27e4c2b52acc9eaa2ddef7
Tahar Jaafer <124438260+Znullptr@users.noreply.github.com>
2024-03-20 01:31:39 +0100

    Update urls.py

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -5,7 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register('MyAPI', views.ApprovalsView)
 urlpatterns = [
-    path('', views.cxcontact, name='Form'),
-    path('status/', views.cxstatus, name='Status'),
-    path('api/', include(router.urls)),
+  # path('', views.cxcontact, name='Form'),
+  # path('status/', views.cxstatus, name='Status'),
+    path('', include(router.urls)),
 ]
